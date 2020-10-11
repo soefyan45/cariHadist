c57b05c481746e2640c566294a729fa817d25441
soefyan <soefyan45@gmail.com>
2020-10-11 23:27:00 +0700

    first comm

---
+++ components/Layouts/header.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+    <header class="h-16 sm:flex sm:items-center sm:justify-between mx-5 lg:mb-1 mt-1">
+        <div id="header">
+            <div class="flex bg-teal-500 border-b border-teal-700 fixed top-0 inset-x-0 z-100 h-18 md:h-16 items-center">
+                <div class="w-full max-w-screen-xl relative mx-auto px-6">
+                    <div class="flex items-center py-5">
+                        <div class="lg:w-1/4 xl:w-1/5 md:pr-2 lg:pr-8">
+                            <div class="flex items-center">
+                                <nuxt-link class="block lg:mr-4 no-underline" to="/">
+                                    <h4 class="text-xl font-mono text-black">
+                                        CariHadist.my.id
+                                    </h4>
+                                </nuxt-link>
+                            </div>
+                        </div>
+                        <div class="flex flex-grow min-w-0 lg:w-3/4 xl:w-4/5">
+                            <div class="w-full min-w-0 lg:px-6 xl:w-3/4 xl:px-12 pr-2">
+                                <div class="relative">
+                                    <!-- search form -->
+                                </div>
+                            </div>
+                            <button @click.prevent="clickToggle" class="inline-flex items-center justify-center p-2 rounded-md lg:hidden text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white">
+                                <!-- Menu open: "hidden", Menu closed: "block" -->
+                                <svg class="block h-6 w-6" v-if="!toggleMenu" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                </svg>
+                                <!-- Menu open: "block", Menu closed: "hidden" -->
+                                <svg class="fill-current w-4 h-4" v-if="toggleMenu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"></path>
+                                </svg>
+                            </button>
+                            <div class="hidden lg:flex lg:items-center lg:justify-between xl:w-1/4 px-6">
+                                <div class="flex justify-start items-center text-gray-500">
+                                    <nuxt-link class="block flex no-underline items-center font-semibold hover:text-black  text-gray-700" to="/about">
+                                        About
+                                    </nuxt-link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="w-full lg:hidden lg:items-center lg:w-auto bg-teal-600 pt-2 md:pt-1 rounded-b-lg" v-if="toggleMenu">
+        <div class="flex text-sm lg:flex-grow py-3 mr-2 justify-end" @click.prevent="unToggle">
+            <nuxt-link class="inline-block border border-white rounded py-2 px-4 bg-teal-500 text-white mr-2" to="/">
+                Home
+            </nuxt-link>
+            <nuxt-link class="inline-block border border-white rounded py-2 px-4 bg-teal-500 text-white mr-2" to="/about">
+                About
+            </nuxt-link>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            toggleMenu: false
+        }
+    },
+    methods: {
+        clickToggle() {
+            if (this.toggleMenu === false) {
+                this.toggleMenu = true
+                /* eslint no-useless-return: "error" */
+                return true
+            }
+            if (this.toggleMenu === true) {
+                this.toggleMenu = false
+                /* eslint no-useless-return: "error" */
+                return false
+            }
+        },
+        unToggle() {
+            console.log('false')
+            this.toggleMenu = false
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,164 @@
+<template>
+<div class="container mx-auto">
+    <div class="row">
+        <div class="w-full max-w-screen-xl mx-auto px-auto">
+            <p class="text-red-500 text-sm">Ketik hadist apa yang akan kamu cari, kemudian klik Cari Hadist</p>
+            <div class="w-full max-w-screen-xl">
+                <div class="flex items-center border-b border-teal-500 py-2">
+                    <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" v-model="cariHadist" placeholder="Niat" v-on:keyup.enter.prevent="clickCari">
+                    <button v-bind:class="{ 'animate-pulse': proseCari }" @click.prevent="clickCari" class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                        {{ textCari }}
+                    </button>
+                    <button v-bind:class="{ 'hidden': !proseCari }" @click="stopCari" class="flex-shrink-0 bg-red-500 hover:bg-red-700 border-red-500 hover:border-red-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                        X
+                    </button>
+                </div>
+            </div>
+            <div v-if="proseCari" class="mt-4 border border-gray-300 shadow rounded-md p-4 max-w-screen-xl">
+                <div class="animate-pulse flex space-x-4">
+                    <div class="flex-1 space-y-4 py-1">
+                        <div class="h-4 bg-gray-400 rounded w-3/4"></div>
+                        <div class="space-y-2">
+                            <div class="h-4 bg-gray-400 rounded"></div>
+                            <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="animate-pulse flex space-x-4">
+                    <div class="flex-1 space-y-4 py-1">
+                        <div class="h-4 bg-gray-400 rounded w-3/4"></div>
+                        <div class="space-y-2">
+                            <div class="h-4 bg-gray-400 rounded"></div>
+                            <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-for="(hadist,index) in Hadist.data" :key="index" class="mt-2 border-green-400 border-2 rounded-sm">
+                <a class="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
+                    <svg class="flex-shrink-0 h-6 w-6 text-tael-300" x-description="Heroicon name: view-grid" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                    </svg>
+                    <div class="row w-full">
+                        <div class="space-y-1 mb-2">
+                            <p class="text-base leading-6 font-medium text-gray-900">
+                                {{hadist.kitab}}
+                            </p>
+                            <p class="text-sm leading-5 text-gray-500">
+                                Nomor Hadist.
+                            </p>
+                        </div>
+                        <div class="grid grid-cols-3 gap-4 pb-3 items-center">
+                            <nuxt-link :to="{path: '/'+hadist.kitab+'/'+idKitab}" type="button" class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded mr-2" v-for="(idKitab,index) in hadist.id" :key="index">{{idKitab}}</nuxt-link>
+                        </div>
+                    </div>
+
+                </a>
+            </div>
+            <div v-if="failedFind" class="lg:text-center pt-3">
+                <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Respone Server</p>
+                <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-red-700 sm:text-4xl sm:leading-10">
+                    Hadist Tidak Di Temukan !!!
+                </h3>
+                <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+                    ketik kata pencarian hadist dengan ejaan yang benar !!!
+                </p>
+            </div>
+            <div v-if="serverUnrespone" class="lg:text-center pt-3">
+                <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Server Tidak Merespone</p>
+                <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                    Silahkan Cek Koneksi Internet Anda !!!
+                </h3>
+                <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+                    cara mudah mencari hadist di carihadis.my.id
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    data() {
+        return {
+            proseCari: false,
+            cariHadist: '',
+            textCari: 'Cari Hadist',
+            Hadist: [],
+            failedFind: false,
+            serverUnrespone: false
+        }
+    },
+    methods: {
+        clickCari() {
+            if (this.cariHadist && this.proseCari == false) {
+                this.proseCari = true
+                this.failedFind = false
+                this.serverUnrespone = false
+                this.textCari = 'Proses Mencari Hadist'
+                this.getHadist()
+            }
+        },
+        stopCari() {
+            this.proseCari = false
+            this.textCari = 'Cari Hadist'
+        },
+        async getHadist() {
+            this.Hadist = []
+            const proxyurl = "https://cors-anywhere.herokuapp.com/";
+            const url = "http://api.carihadis.com/" + "?q=" + this.cariHadist; // site that doesn’t send Access-Control-*
+            await fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+                .then(response => response.text())
+                .then(contents => {
+                    //console.log(contents)
+                    this.proseCari = false
+                    this.textCari = 'Cari Hadist'
+                    this.Hadist = JSON.parse(contents)
+                    //console.log(JSON.parse(contents))
+                    if (JSON.parse(contents).data == null) {
+                        //alert('Data Tidak Di Temukan')
+                        this.failedFind = true
+                    }
+                })
+                .catch((e) => {
+                  this.serverUnrespone = true
+                })
+        },
+        async nomorHadist(kitab) {
+            //alert(kitab)
+            if (process.server) {
+                return
+            } else {
+                return this.Hadist.filter((kitab) => {
+                    console.log(this.Hadist.match(this.Hadist))
+                    return this.Hadist.match(this.Hadist)
+                })
+            }
+
+        }
+    }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.animation.spin {
+    animation: spin 1s linear infinite;
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+
+        to {
+            transform: rotate(360deg);
+        }
+    }
+}
+</style>
